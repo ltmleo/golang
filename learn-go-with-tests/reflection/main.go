95ea1cdb5a242943b005d4cd9e577da32246d231
Leonardo Montero <leonardo.tmontero@telefonica.com>
2021-08-22 13:16:42 -0300

    add reflection

--- learn-go-with-tests/reflection/main.go
+++ learn-go-with-tests/reflection/main.go
@@ -15,16 +15,25 @@ func walk(x interface{}, fn func(input string)) {
     case reflect.String:
         fn(val.String())
     case reflect.Struct:
-        for i := 0; i< val.NumField(); i++ {
+        for i := 0; i < val.NumField(); i++ {
             walkValue(val.Field(i))
         }
     case reflect.Slice, reflect.Array:
-        for i:= 0; i<val.Len(); i++ {
+        for i := 0; i < val.Len(); i++ {
             walkValue(val.Index(i))
         }
     case reflect.Map:
         for _, key := range val.MapKeys() {
             walkValue(val.MapIndex(key))
+        }
+    case reflect.Chan:
+        for v, ok := val.Recv(); ok; v, ok = val.Recv() {
+            walkValue(v)
+        }
+    case reflect.Func:
+        valFnResult := val.Call(nil)
+        for _, res := range valFnResult {
+            walkValue(res)
         }
     }
 }
