0a82198f5a7f66b408668c265a5f7781e79c35b1
Leonardo Montero <leonardo.tmontero@telefonica.com>
2024-07-11 18:06:12 -0300

    ok version

--- file-assync/main.go
+++ file-assync/main.go
@@ -1,81 +1,44 @@
 package main
 
 import (
-    "bufio"
-    "fmt"
-    "io"
-    "os"
-    "sync"
-    "time"
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"time"
 )
 
-
-func readLogFile(logFile string, ch chan<- string, wg *sync.WaitGroup) {
-    defer wg.Done()
-
-    file, err := os.Open(logFile)
-    if err != nil {
-        fmt.Println("Error opening log file:", err)
-        return
-    }
-    defer file.Close()
-    scanner := bufio.NewScanner(file)
-    for scanner.Scan() {
-        line := scanner.Text()
-        ch <- line
-    }
-    if err := scanner.Err(); err != nil {
-        fmt.Println("Error reading log file:", err)
-    }
-    for {
-        stat, err := file.Stat()
-        if err != nil {
-            fmt.Println("Error getting file stats:", err)
-            return
-        }
-        lastModTime := stat.ModTime()
-        time.Sleep(time.Second) 
-        stat, err = file.Stat()
-        if err != nil {
-            fmt.Println("Error getting file stats:", err)
-            return
-        }
-        if stat.ModTime().After(lastModTime) {
-            file.Seek(0, io.SeekCurrent)
-            scanner = bufio.NewScanner(file)
-
-            for scanner.Scan() {
-                line := scanner.Text()
-                ch <- line
-            }
-
-            if err := scanner.Err(); err != nil {
-                fmt.Println("Error reading log file:", err)
-            }
-        }
-    }
+func tail(filename string, out io.Writer, done chan bool) {
+	f, err := os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	r := bufio.NewReader(f)
+	for {
+		select {
+		case <-done:
+			return
+		default:
+			for line, prefix, err := r.ReadLine(); err != io.EOF; line, prefix, err = r.ReadLine() {
+				if prefix {
+					fmt.Fprint(out, filename, string(line))
+				} else {
+					fmt.Fprintln(out, filename, string(line))
+				}
+			}
+		}
+	}
 }
-func executeFunction() {
-	time.Sleep(time.Second*10)
+
+func function(done chan bool) {
+	time.Sleep(time.Second * 10)
+	done <- true
 }
 
 func main() {
-    ch := make(chan string)
-	ch2 := make(chan string)
-    wg := sync.WaitGroup{}
-    wg.Add(1)
-    go readLogFile("file.txt", ch, &wg)
-    for line := range ch {
-        fmt.Println("Log line:", line)
-	}
-	wg.Add(1)
-	go readLogFile("file2.txt", ch2, &wg)
-    for line := range ch2 {
-        fmt.Println("Log line 2:", line)
-	}
-    wg.Add(1)
-    go executeFunction()
-    wg.Wait()
-
-    fmt.Println("Log file processing complete.")
+	done := make(chan bool)
+	go tail("file.txt", os.Stdout, done)
+	go tail("file2.txt", os.Stdout, done)
+	function(done)
 }
